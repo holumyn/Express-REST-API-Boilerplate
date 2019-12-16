a9ac23850806f7e57c7b84e45c54a622ea1877bd
Olumide <noble2016@gmail.com>
2019-12-16 16:39:35 +0100

    Added support for creating users

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
 const appRoot = require('app-root-path');
 const userHandler = require(`${appRoot}/api/controllers/userController`);
 const commonHandler = require(`${appRoot}/api/controllers/commonController`);
+const { ACCOUNT_TYPE } = require(`${appRoot}/api/constants/constants`);
 
 module.exports = function userRoute (router) {
   router.route('/user')
     .post(userHandler.register)
-    .get(commonHandler.protected, userHandler.getProfile);
+    .get(commonHandler.authorize(ACCOUNT_TYPE.REGULAR), userHandler.getProfile);
 };
